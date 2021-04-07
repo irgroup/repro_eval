003b40514ea4c08994183264681f542f4d01913c
breuert <timo.breuer@th-koeln.de>
2021-04-07 18:43:58 +0200

    add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(name='repro_eval',
       version='0.1',
       description='A tool to quantify the replicability and reproducibility of system-oriented IR experiments.',
+      long_description=README,
+      long_description_content_type="text/markdown",
       url='http://github.com/irgroup/repro_eval',
       author='Timo Breuer',
       author_email='timo.breuer@th-koeln.de',
