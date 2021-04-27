1a03bde7db523d8483308a301d637f7a6ebc01c3
breuert <timo.breuer@th-koeln.de>
2021-04-27 17:49:37 +0200

    test ttest with identical score distributions

---
+++ repro_eval/test/test_ttest.py
@@ -0,0 +1,21 @@
+import pytest
+from repro_eval.Evaluator import RpdEvaluator
+
+
+def test_ttest_with_identical_score_distributions():
+    rpd_eval = RpdEvaluator(qrel_orig_path='./example/data/qrels/core17.txt',
+                            run_b_orig_path='./example/orig_b.txt',
+                            run_a_orig_path='./example/orig_a.txt',
+                            run_b_rep_path='./example/orig_b.txt',
+                            run_a_rep_path='./example/orig_a.txt')
+
+    rpd_eval.trim()
+    rpd_eval.evaluate()
+
+    ttest = rpd_eval.ttest()
+
+    pvals = list(filter(lambda x: x == 1.0, ttest.get('baseline').values()))
+    assert len(pvals) == len(ttest.get('baseline').keys())
+
+    pvals = list(filter(lambda x: x == 1.0, ttest.get('advanced').values()))
+    assert len(pvals) == len(ttest.get('advanced').keys())
