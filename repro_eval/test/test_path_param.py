4150059e5c8963e31de6ad0598ddd1a3b8fbc1fd
breuert <timo.breuer@th-koeln.de>
2021-05-28 15:31:45 +0200

    test optional path params of ttest

--- repro_eval/test/test_path_param.py
+++ repro_eval/test/test_path_param.py
@@ -1,5 +1,6 @@
 import pytest
 from repro_eval.Evaluator import RpdEvaluator
+import numpy as np
 
 rpd_eval = RpdEvaluator(qrel_orig_path='./example/data/qrels/core17.txt',
                         run_b_orig_path='./example/orig_b.txt',
@@ -69,3 +70,29 @@ def test_rmse_path_param():
     _rmse = _rpd_eval.rmse(run_b_path='./example/rpd_b.txt', run_a_path='./example/rpd_a.txt')
     assert 'advanced' in _rmse.keys()
     assert rmse.get('advanced') == _rmse.get('advanced')
+
+
+def test_rpd_ttest_path_param():
+    pval = rpd_eval.ttest()
+    assert 'baseline' in pval.keys()
+    assert 'advanced' in pval.keys()
+
+    _rpd_eval = RpdEvaluator(qrel_orig_path='./example/data/qrels/core17.txt',
+                             run_b_orig_path='./example/orig_b.txt',
+                             run_a_orig_path='./example/orig_a.txt')
+    _rpd_eval.trim()
+    _rpd_eval.evaluate()
+
+    _pval = _rpd_eval.ttest(run_b_path='./example/rpd_b.txt')
+    assert 'baseline' in _pval.keys()
+    # pick a few samples here since nan comparisons cause problems in combination with assert
+    assert pval.get('baseline').get('ndcg') == _pval.get('baseline').get('ndcg')
+    assert pval.get('baseline').get('P_10') == _pval.get('baseline').get('P_10')
+    assert pval.get('baseline').get('map') == _pval.get('baseline').get('map')
+
+    _pval = _rpd_eval.ttest(run_b_path='./example/rpd_b.txt', run_a_path='./example/rpd_a.txt')
+    assert 'advanced' in _pval.keys()
+    # pick a few samples here since nan comparisons cause problems in combination with assert
+    assert pval.get('advanced').get('ndcg') == _pval.get('advanced').get('ndcg')
+    assert pval.get('advanced').get('P_10') == _pval.get('advanced').get('P_10')
+    assert pval.get('advanced').get('map') == _pval.get('advanced').get('map')
