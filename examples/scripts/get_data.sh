694b25eb593a5d2b1397c9844cd07d6811cc1a6a
breuert <timo.breuer@th-koeln.de>
2025-11-26 21:09:08 +0100

    update download script.

--- examples/scripts/get_data.sh
+++ examples/scripts/get_data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 wget https://www.dropbox.com/s/p1wwqqka1n3el6b/runs.tar.gz 
-tar -xzvf runs.tar.gz -C ./example/data/
+mkdir ./data
+mkdir ./data/qrels
+wget -O "./data/qrels/core17.txt" https://trec.nist.gov/data/core/qrels.txt
+wget -O "./data/qrels/core18.txt" https://trec.nist.gov/data/core/qrels2018.txt
+tar -xzvf runs.tar.gz -C ./data/
